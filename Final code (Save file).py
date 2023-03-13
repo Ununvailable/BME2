1541b8ff9bb4ef7b56492b7c2e75c64ff1b9ee63
Ununvailable <103551936+Ununvailable@users.noreply.github.com>
2023-03-13 15:11:04 +0700

    Update Final code (Save file).py

--- Final code (Save file).py
+++ Final code (Save file).py
@@ -36,100 +36,96 @@ ax.set_zlabel('Z')
 ser = serial.Serial("COM5", baudrate=115200)
 ser.flushInput()
 
-try:
-    with open("Angle_Three_sensor.csv", "a") as f:
-        label = ['Angle1', 'Angle1/2', 'Angle2/3', 'Angle3/4', 'Angle4/5',
-                 'X1', 'Y1', 'Z1',
-                 'X2', 'Y2', 'Z2',
-                 'X3', 'Y3', 'Z3',
-                 'X4', 'Y4', 'Z4',
-                 'X5', 'Y5', 'Z5',]
-        writer = csv.writer(f)
-        # print("Start reading")
-        # for i in range(20):
-        #     i += 1
-        #     label.append(i)
-        writer.writerow(label)
+with open("Angle_Three_sensor.csv", "a") as f:
+    label = ['Angle1', 'Angle1/2', 'Angle2/3', 'Angle3/4', 'Angle4/5',
+             'X1', 'Y1', 'Z1',
+             'X2', 'Y2', 'Z2',
+             'X3', 'Y3', 'Z3',
+             'X4', 'Y4', 'Z4',
+             'X5', 'Y5', 'Z5',]
+    writer = csv.writer(f)
+    # print("Start reading")
+    # for i in range(20):
+    #     i += 1
+    #     label.append(i)
+    writer.writerow(label)
 
-        while True:
-            def move_arrow(pos1, direction1, direction2, direction3, direction4, direction5,
-                           trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,
-                           length_trunk,
-                           ):
-                # Converting directional inputs into arrays
-                pos1 = np.asarray(pos1)
-                direction1 = np.asarray(direction1)
-                direction2 = np.asarray(direction2)
-                direction3 = np.asarray(direction3)
-                direction4 = np.asarray(direction4)
-                direction5 = np.asarray(direction5)
+    while True:
+        def move_arrow(pos1, direction1, direction2, direction3, direction4, direction5,
+                       trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,
+                       length_trunk,
+                       ):
+            # Converting directional inputs into arrays
+            pos1 = np.asarray(pos1)
+            direction1 = np.asarray(direction1)
+            direction2 = np.asarray(direction2)
+            direction3 = np.asarray(direction3)
+            direction4 = np.asarray(direction4)
+            direction5 = np.asarray(direction5)
 
-                # Normalizing
-                direction1 = direction1 / np.linalg.norm(direction1)
-                direction2 = direction2 / np.linalg.norm(direction2)
-                direction3 = direction3 / np.linalg.norm(direction3)
-                direction4 = direction4 / np.linalg.norm(direction4)
-                direction5 = direction5 / np.linalg.norm(direction5)
+            # Normalizing
+            direction1 = direction1 / np.linalg.norm(direction1)
+            direction2 = direction2 / np.linalg.norm(direction2)
+            direction3 = direction3 / np.linalg.norm(direction3)
+            direction4 = direction4 / np.linalg.norm(direction4)
+            direction5 = direction5 / np.linalg.norm(direction5)
 
-                # Shifting vectors
-                shift1 = direction1 * length_trunk[0]
-                end1 = pos1 + shift1
-                shift2 = direction2 * length_trunk[1]
-                end2 = end1 + shift2
-                shift3 = direction3 * length_trunk[2]
-                end3 = end2 + shift3
-                shift4 = direction4 * length_trunk[3]
-                end4 = end3 + shift4
-                shift5 = direction5 * length_trunk[4]
-                end5 = end4 + shift5
+            # Shifting vectors
+            shift1 = direction1 * length_trunk[0]
+            end1 = pos1 + shift1
+            shift2 = direction2 * length_trunk[1]
+            end2 = end1 + shift2
+            shift3 = direction3 * length_trunk[2]
+            end3 = end2 + shift3
+            shift4 = direction4 * length_trunk[3]
+            end4 = end3 + shift4
+            shift5 = direction5 * length_trunk[4]
+            end5 = end4 + shift5
 
-                axis = np.array([0, 0, 1])
-                ang1 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction1, axis), -1.0, 1.0))), 2)
-                ang2 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction2, direction1), -1.0, 1.0))), 2)
-                ang3 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction3, direction2), -1.0, 1.0))), 2)
-                ang4 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction4, direction3), -1.0, 1.0))), 2)
-                ang5 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction5, direction4), -1.0, 1.0))), 2)
-                print("Root angle: ", ang1, "Angle 1/2", ang2, "Ang 2/3", ang3, "Ang 3/4", ang4, "Ang 4/5", ang5)
-                writer.writerow([ang1, ang2, ang3, ang4, ang5,
-                                 direction1[0], direction1[1], direction1[2],
-                                 direction2[0], direction2[1], direction2[2],
-                                 direction3[0], direction3[1], direction3[2],
-                                 direction4[0], direction4[1], direction4[2],
-                                 direction5[0], direction5[1], direction5[2],
-                                 ])
+            axis = np.array([0, 0, 1])
+            ang1 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction1, axis), -1.0, 1.0))), 2)
+            ang2 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction2, direction1), -1.0, 1.0))), 2)
+            ang3 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction3, direction2), -1.0, 1.0))), 2)
+            ang4 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction4, direction3), -1.0, 1.0))), 2)
+            ang5 = round(np.rad2deg(np.arccos(np.clip(np.dot(direction5, direction4), -1.0, 1.0))), 2)
+            print("Root angle: ", ang1, "Angle 1/2", ang2, "Ang 2/3", ang3, "Ang 3/4", ang4, "Ang 4/5", ang5)
+            writer.writerow([ang1, ang2, ang3, ang4, ang5,
+                             direction1[0], direction1[1], direction1[2],
+                             direction2[0], direction2[1], direction2[2],
+                             direction3[0], direction3[1], direction3[2],
+                             direction4[0], direction4[1], direction4[2],
+                             direction5[0], direction5[1], direction5[2],
+                             ])
 
-                trunk_plt1.set_data([pos1[0], end1[0]], [pos1[1], end1[1]])  # Set data
-                trunk_plt1.set_3d_properties([pos1[2], end1[2]])  # Set properties
+            trunk_plt1.set_data([pos1[0], end1[0]], [pos1[1], end1[1]])  # Set data
+            trunk_plt1.set_3d_properties([pos1[2], end1[2]])  # Set properties
 
-                trunk_plt2.set_data([end1[0], end2[0]], [end1[1], end2[1]])
-                trunk_plt2.set_3d_properties([end1[2], end2[2]])
+            trunk_plt2.set_data([end1[0], end2[0]], [end1[1], end2[1]])
+            trunk_plt2.set_3d_properties([end1[2], end2[2]])
 
-                trunk_plt3.set_data([end2[0], end3[0]], [end2[1], end3[1]])
-                trunk_plt3.set_3d_properties([end2[2], end3[2]])
+            trunk_plt3.set_data([end2[0], end3[0]], [end2[1], end3[1]])
+            trunk_plt3.set_3d_properties([end2[2], end3[2]])
 
-                trunk_plt4.set_data([end3[0], end4[0]], [end3[1], end4[1]])
-                trunk_plt4.set_3d_properties([end3[2], end4[2]])
+            trunk_plt4.set_data([end3[0], end4[0]], [end3[1], end4[1]])
+            trunk_plt4.set_3d_properties([end3[2], end4[2]])
 
-                trunk_plt5.set_data([end4[0], end5[0]], [end4[1], end5[1]])
-                trunk_plt5.set_3d_properties([end4[2], end5[2]])
-
+            trunk_plt5.set_data([end4[0], end5[0]], [end4[1], end5[1]])
+            trunk_plt5.set_3d_properties([end4[2], end5[2]])
 
 
-            def update(i):
-                data = ser.readline().decode()[0:][:-2].split(",")  # 2-1-0/5-4-3/8-7-6
-                dir_shift1 = np.array([float(data[2]) - error1[0], float(data[1]) - error1[1], float(data[0]) - error1[2]])
-                dir_shift2 = np.array([float(data[5]) - error2[0], float(data[4]) - error2[1], float(data[3]) - error2[2]])
-                dir_shift3 = np.array([float(data[8]) - error3[0], float(data[7]) - error3[1], float(data[6]) - error3[2]])
-                dir_shift4 = np.array([float(data[11]) - error4[0], float(data[10]) - error4[1], float(data[9]) - error4[2]])
-                dir_shift5 = np.array([float(data[14]) - error5[0], float(data[13]) - error5[1], float(data[12]) - error5[2]])
-                move_arrow(pos1,dir1 + dir_shift1,dir2 + dir_shift2,dir3 + dir_shift3,dir4 + dir_shift4,dir5 + dir_shift5,
-                           trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,
-                           length_trunk)
-                return [trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,]
 
-            ani = animation.FuncAnimation(fig, update, frames=None, interval=10, repeat=True, blit=True)
+        def update(i):
+            data = ser.readline().decode()[0:][:-2].split(",")  # 2-1-0/5-4-3/8-7-6
+            dir_shift1 = np.array([float(data[2]) - error1[0], float(data[1]) - error1[1], float(data[0]) - error1[2]])
+            dir_shift2 = np.array([float(data[5]) - error2[0], float(data[4]) - error2[1], float(data[3]) - error2[2]])
+            dir_shift3 = np.array([float(data[8]) - error3[0], float(data[7]) - error3[1], float(data[6]) - error3[2]])
+            dir_shift4 = np.array([float(data[11]) - error4[0], float(data[10]) - error4[1], float(data[9]) - error4[2]])
+            dir_shift5 = np.array([float(data[14]) - error5[0], float(data[13]) - error5[1], float(data[12]) - error5[2]])
+            move_arrow(pos1,dir1 + dir_shift1,dir2 + dir_shift2,dir3 + dir_shift3,dir4 + dir_shift4,dir5 + dir_shift5,
+                       trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,
+                       length_trunk)
+            return [trunk_plt1, trunk_plt2, trunk_plt3, trunk_plt4, trunk_plt5,]
 
-            plt.show()
+        ani = animation.FuncAnimation(fig, update, frames=None, interval=10, repeat=True, blit=True)
 
-except:
-    print("Keyboard Interrupt")
+        plt.show()
